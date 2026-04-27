2578a492fff805668f22ec1c3292dfb15f7f112d
SouheilMs <souheil000@gmail.com>
2026-04-27 17:09:30 +0100

    fix issues

--- Planora.Infrastructure/Settings/EmailSettings.cs
+++ Planora.Infrastructure/Settings/EmailSettings.cs
@@ -2,11 +2,11 @@ namespace Planora.Infrastructure.Settings;
 
 public class EmailSettings
 {
-    public string SmtpHost { get; set; } = string.Empty;
+    public string SmtpHost { get; set; } = "smtp.gmail.com";
     public int SmtpPort { get; set; } = 587;
-    public string SenderEmail { get; set; } = string.Empty;
+    public string SenderEmail { get; set; } = "planora@gmail.com";
     public string SenderName { get; set; } = "Planora";
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public string Username { get; set; } = "planora@gmail.com";
+    public string Password { get; set; } = "sbmclnnwuvlyppgt";
     public bool EnableSsl { get; set; } = true;
 }
